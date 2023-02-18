4a3adaf4ee8e1c19af7ca097fb4cd0cb0daf7581
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-02-18 15:28:07 +0200

    finished task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
