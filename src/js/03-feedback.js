7c29ab16e08b016924e5a2dd50ccb06d2a396c08
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-02-18 23:20:03 +0200

    finished task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import Throttle from 'lodash.throttle';
 
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+
+formRef.addEventListener('input', Throttle(onFormInputs, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+fillFormFields();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInputs(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function fillFormFields() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData) {
+    formRef.firstElementChild.children.email.value = savedFormData.email;
+    formRef.firstElementChild.nextElementSibling.children.message.value =
+      savedFormData.message;
+  }
+}
