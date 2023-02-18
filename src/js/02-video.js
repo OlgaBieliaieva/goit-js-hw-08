b553072f8662b916fec4b1f62fe3e0b9c44a35e3
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-02-18 20:13:16 +0200

    finished task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeRef);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+setStartTime();
+
+player.on(
+  'timeupdate',
+  Throttle(function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem(STORAGE_KEY, currentTime);
+  }, 1000)
+);
+
+function setStartTime() {
+  const startTime = localStorage.getItem(STORAGE_KEY);
+  if (startTime) {
+    player.setCurrentTime(startTime);
+  }
+}
